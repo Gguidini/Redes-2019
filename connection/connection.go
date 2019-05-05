0b391374c2747e7ea0e4e6b66e3a04e9328ed630
Gguidini <giovanni.guidini@gmail.com>
2019-05-05 15:23:02 -0300

    Adicionando checagem de autenticação.
    - Cliente para se autenticação com o servidor não for bem sucedida.
    - Mensagens de welcome são mostradas quando acabar de autenticar
    - Verifica socket quando abre a conexão (antes de autenticar)

--- connection/connection.go
+++ connection/connection.go
@@ -21,58 +21,90 @@ func NewClient(socket net.Conn, userInfo userinterface.User, connInfo userinterf
 	return &IrcClient{socket, userInfo, connInfo, make(chan string, 100)}
 }
 
+// OpenSocket abre uma conexão com o servidor IRC
+// Recebe as informações de conexão (userinterface.ConnInfo)
+// Retorna o socket, caso tudo aconteça bem.
 func OpenSocket(conn userinterface.ConnInfo) net.Conn {
+	// Servidor:Porta
 	connTarget := conn.Servername + ":" + strconv.Itoa(conn.Port)
 	fmt.Println("\nAbrindo o socket TCP para", connTarget)
 	connSocket, err := net.Dial("tcp", connTarget)
 	if err != nil {
 		panic(err)
 	} else {
+		// Quando uma conexão é aberta, o servidor tenta encontrar o usuário.
+		// Vamos ler as mensagens de retorno para ver se o socket está ok
+		// E para limpar o buffer de entrada
+		// Como não passamos nenhum usuário, duas mensagens são enviadas:
+		// :server_name NOTICE * :*** Looking up your username
+		// :server_name NOTICE * :*** Could not find your username
+		reader := bufio.NewReader(connSocket)
+		msgLookUp, errLookUp := reader.ReadString('\n')
+		_, errUserNotFound := reader.ReadString('\n')
+		if errLookUp != nil {
+			panic(errLookUp)
+		} else if errUserNotFound != nil {
+			panic(errUserNotFound)
+		}
+		server := strings.Fields(msgLookUp)[0]
+		fmt.Println("Connection opened to", server[1:])
 		return connSocket
 	}
 }
 
-// func
-func (client *IrcClient) Connect(user userinterface.User) {
+// Connect faz a autenticação com o servidor IRC com o qual temos um socket
+// O cliente já tem o socket aberto com o servidor
+// O cliente também já possui as informações de usuário e de conexão
+// Autenticação é feita em 3 comandos:
+// 1. PASS 2. NICK 3. USER
+func (client *IrcClient) Connect() bool {
 	fmt.Println("Autenticando com o servidor")
 	// Inicialmente manda PASS, se for necessário
 	if client.connInfo.HasPasswd {
-		passCmd := "PASS " + client.connInfo.Passwd + "\r\n"
-		_, err := client.Socket.Write([]byte(passCmd))
+		pass := passCmd(client.connInfo.Passwd)
+		_, err := client.Socket.Write([]byte(pass))
 		if err != nil {
-			// fmt.Println("Erro enviando PASS")
-			// fmt.Println("Enviado:", passCmd)
-			// fmt.Println("Erro:", err)
 			panic(err)
-			return
 		}
 	}
 
 	// Manda NICK
-	client.UserInfo = user
-	nickCmd := "NICK " + client.UserInfo.Nick + "\r\n"
-	client.Socket.Write([]byte(nickCmd))
-	_, err := client.Socket.Write([]byte(nickCmd))
+	nick := nickCmd(client.UserInfo.Nick)
+	_, err := client.Socket.Write([]byte(nick))
 	if err != nil {
-		fmt.Println("Erro enviando NICK")
-		fmt.Println("Enviado:", nickCmd)
-		fmt.Println("Erro:", err)
-		return
+		panic(err)
 	}
 	// Manda USER
-	userCmd := ("USER " +
-		client.UserInfo.Nick + " " +
-		client.UserInfo.Hostname + " " +
-		client.connInfo.Servername + " :" +
-		client.UserInfo.Username + "\r\n")
-	client.Socket.Write([]byte(userCmd))
-	_, err = client.Socket.Write([]byte(userCmd))
+	user := userCmd(
+		client.UserInfo.Nick,
+		client.UserInfo.Hostname,
+		client.connInfo.Servername,
+		client.UserInfo.Username)
+	_, err = client.Socket.Write([]byte(user))
 	if err != nil {
-		fmt.Println("Erro enviando USER")
-		fmt.Println("Enviado:", userCmd)
-		fmt.Println("Erro:", err)
-		return
+		panic(err)
 	}
+
+	// Verifica se o servidor enviou as mensagens de Welcome.
+	// Caso elas não tenham sido recebidas, autenticação falhou.
+	reader := bufio.NewReader(client.Socket)
+	msg, _ := reader.ReadString('\n')
+	msgPieces := strings.Fields(strings.TrimRight(msg, CRLF))
+	if msgPieces[1] != "001" {
+		fmt.Println("Erro de Autenticação:", strings.Join(msgPieces[1:], " "))
+		client.Socket.Close()
+		return false
+	}
+
+	// Autenticação foi bem sucedida. Mostra mensagens de boas-vindas
+	fmt.Println("Autenticação bem sucedida!")
+	fmt.Println(strings.Join(msgPieces[3:], " "))
+	for i := 0; i < 4; i++ {
+		msg, _ = reader.ReadString('\n')
+		msgPieces = strings.Fields(strings.TrimRight(msg, CRLF))
+		fmt.Println(strings.Join(msgPieces[3:], " "))
+	}
+	return true
 }
 
 func (client *IrcClient) Receive() {
@@ -98,10 +130,11 @@ func (client *IrcClient) Receive() {
 			// Mensagens de PING devem ser respondidas para manter o canal aberto.
 			// A resposta é um PONG
 			fmt.Println("Received PING. Responding.")
-			reply := "PONG " + fields[1] + "\r\n"
+			reply := pongCmd(fields[1])
 			client.Socket.Write([]byte(reply))
-		} else if fields[0] == "ERROR" {
+		} else if fields[0] == "ERROR" || fields[0] == "KILL" {
 			// Mensagens de ERROR significam que algo deu errado e o servidor fechou a conexão
+			// KILL significa que a conexão foi fechada por algum operador
 			// Logo, o cliente precisará se reconectar.
 			fmt.Println("Fatal Error:", message)
 			client.Socket.Close()
