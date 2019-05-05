09976f940faaa83bb50111241749ec2380059785
Gguidini <giovanni.guidini@gmail.com>
2019-05-04 23:41:45 -0300

    Integrando cliente IRC v0.1

--- ircclient/ircclient.go
+++ ircclient/ircclient.go
@@ -1,14 +1,101 @@
 package main
+
 import (
+	"bufio"
 	"fmt"
+	"net"
+	"os"
+	"strconv"
+	"strings"
+
+	"github.com/Redes-2019/userinterface"
 )
 
-import "github.com/Redes-2019/userinterface"
+type ircClient struct {
+	socket   net.Conn
+	userInfo userinterface.User
+	connInfo userinterface.ConnInfo
+}
 
 func main() {
-	user := new(userinterface.User)
-	user = userinterface.ReadUserData()
-	fmt.Print((*user).Username)
+	fmt.Println("Iniciando Cliente.")
+	fmt.Println("Identificando usuário")
+	user, conn := userinterface.ReadUserData()
+
+	connTarget := conn.Servername + ":" + strconv.Itoa(conn.Port)
+	fmt.Println("\nAbrindo o socket TCP para", connTarget)
+	connSocket, err := net.Dial("tcp", connTarget)
+
+	if err != nil {
+		fmt.Println("Error connecting:", err)
+	}
+	client := &ircClient{socket: connSocket, userInfo: user, connInfo: conn}
+
+	go client.receive()
+	client.connect()
+
+	for {
+		reader := bufio.NewReader(os.Stdin)
+		message, _ := reader.ReadString('\n')
+		strings.TrimRight(message, "\n")
+		message += "\r\n"
+		fmt.Println("Sending to server:", message)
+		connSocket.Write([]byte(message))
+	}
+
 }
 
 // func
+func (client *ircClient) connect() {
+	fmt.Println("Autenticando com o servidor")
+	// Inicialmente manda PASS, se for necessário
+	if client.connInfo.HasPasswd {
+		passCmd := "PASS " + client.connInfo.Passwd + "\r\n"
+		_, err := client.socket.Write([]byte(passCmd))
+		if err != nil {
+			fmt.Println("Erro enviando PASS")
+			fmt.Println("Enviado:", passCmd)
+			fmt.Println("Erro:", err)
+			return
+		}
+	}
+
+	// Manda NICK
+	nickCmd := "NICK " + client.userInfo.Nick + "\r\n"
+	client.socket.Write([]byte(nickCmd))
+	_, err := client.socket.Write([]byte(nickCmd))
+	if err != nil {
+		fmt.Println("Erro enviando NICK")
+		fmt.Println("Enviado:", nickCmd)
+		fmt.Println("Erro:", err)
+		return
+	}
+	// Manda USER
+	userCmd := ("USER " +
+		client.userInfo.Nick + " " +
+		client.userInfo.Hostname + " " +
+		client.connInfo.Servername + " :" +
+		client.userInfo.Username + "\r\n")
+	client.socket.Write([]byte(userCmd))
+	_, err = client.socket.Write([]byte(userCmd))
+	if err != nil {
+		fmt.Println("Erro enviando USER")
+		fmt.Println("Enviado:", userCmd)
+		fmt.Println("Erro:", err)
+		return
+	}
+}
+
+func (client *ircClient) receive() {
+	for {
+		message := make([]byte, 4096)
+		length, err := client.socket.Read(message)
+		if err != nil {
+			client.socket.Close()
+			break
+		}
+		if length > 0 {
+			fmt.Println("RECEIVED: " + string(message))
+		}
+	}
+}
--- userinterface/userinterface.go
+++ userinterface/userinterface.go
@@ -1,29 +1,79 @@
 package userinterface
 
 import (
-	"fmt"
 	"bufio"
+	"fmt"
 	"os"
+	"strconv"
+	"strings"
 )
 
-// Atributos com letra maíuscula podem ser acessados de fora
+// User define as informações de identificação de um cliente IRC.
+// As informações são:
+// Username: nome real do usuário
+// Nick: nome utilizado como identificador
+// Hostname: máquina em que o cliente está rodando
 type User struct {
 	Username string
-	Nick string
-	Password string
+	Nick     string
+	Hostname string
 }
 
-func ReadUserData() *User {
+// ConnInfo define as informações de conexão de um cliente IRC.
+// As informações são:
+// Servername: servidor IRC no qual o cliente vai se conectar
+// HasPasswd: se aquela conexão precisa de uma senha ou não
+// Passwd: senha da conexão se HasPasswd == true, "" se HasPasswd == false
+type ConnInfo struct {
+	Servername string
+	Port       int
+	Passwd     string
+	HasPasswd  bool
+}
+
+// ReadUserData lê as informações do usuário utilizando o cliente IRC
+// As informações são aquelas da struct User e ConnInfo
+func ReadUserData() (User, ConnInfo) {
 	reader := bufio.NewReader(os.Stdin)
-	
-	fmt.Print("username: ")
+	// Username
+	fmt.Print("Username: ")
 	username, _ := reader.ReadString('\n')
-	fmt.Print("nick: ")
+	username = strings.TrimRight(username, "\n")
+	// Nickname
+	fmt.Print("Nick: ")
 	nick, _ := reader.ReadString('\n')
-	fmt.Print("senha: ")
-	password, _ := reader.ReadString('\n')
+	nick = strings.TrimRight(nick, "\n")
+	// Server to connect
+	fmt.Print("Remote server name: ")
+	server, _ := reader.ReadString('\n')
+	server = strings.TrimRight(server, "\n")
+	// Connection port
+	fmt.Print("Porta da conexão: ")
+	port, _ := reader.ReadString('\n')
+	port = strings.TrimRight(port, "\n")
+	numPort, _ := strconv.Atoi(port)
+	// Connection Password, if any
+	fmt.Print("O servidor possui uma senha? [s/n] ")
+	pass, _ := reader.ReadString('\n')
+	pass = strings.TrimRight(pass, "\n")
+	pass = strings.TrimLeft(pass, "\n")
+	passFlag := false
+	if strings.ToLower(pass) == "s" {
+		passFlag = true
+		fmt.Print("Senha: ")
+		pass, _ = reader.ReadString('\n')
+		pass = strings.TrimRight(pass, "\n")
+	} else {
+		passFlag = false
+		pass = ""
+	}
 
-	newUser := User{username, nick, password}
-	return &newUser
+	host, err := os.Hostname()
+	if err != nil {
+		host = "Unkown"
+	}
+
+	newUser := User{username, nick, host}
+	newConnInfo := ConnInfo{server, numPort, pass, passFlag}
+	return newUser, newConnInfo
 }
-
